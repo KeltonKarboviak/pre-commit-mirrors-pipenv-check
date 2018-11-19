b9b39ebf4266e17eff8f727795d62e7a5961d303
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-11-19 01:35:41 +0000

    Mirror: 2018.7.1

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['pipenv==2018.6.25'],
+    install_requires=['pipenv==2018.7.1'],
 )
